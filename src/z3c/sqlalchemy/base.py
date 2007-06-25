3b3697d24a8aec919d3cef9df28111ca43487f0f
Andreas Jung <yet@gmx.de>
2007-06-25 13:08:35 +0000

    typo

--- src/z3c/sqlalchemy/base.py
+++ src/z3c/sqlalchemy/base.py
@@ -107,7 +107,7 @@ class BaseWrapper(object):
 
     @property
     def metadata(self):
-        if not hasattr(self, '_v_metadata':
+        if not hasattr(self, '_v_metadata'):
             self._v_metadata = sqlalchemy.BoundMetaData(self._engine)
         return self._v_metadata
 
