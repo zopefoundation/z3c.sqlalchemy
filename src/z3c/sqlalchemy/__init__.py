5627086019bc5c723ef122f1550620688cb9717c
Andreas Jung <yet@gmx.de>
2007-08-15 14:39:50 +0000

    check for SA 0.4+

--- src/z3c/sqlalchemy/__init__.py
+++ src/z3c/sqlalchemy/__init__.py
@@ -9,3 +9,8 @@
 
 from util import *
 from model import Model
+
+try:
+    from sqlalchemy.orm import sessionmaker
+except ImportError:
+    raise ImportError('z3c.sqlalchemy requires SQLAlchemy 0.4 or higher')
