0f625bac0ab96173c6d50e54d43e063ffefb6b85
Andreas Jung <yet@gmx.de>
2008-08-05 10:40:15 +0000

    updated

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ CLASSIFIERS = [
     'Topic :: Software Development :: Libraries :: Python Modules',
 ]
 
-version = '1.3.2'
+version = '1.3.3'
 
 desc = open('README.txt').read().strip()
 changes = open('CHANGES.txt').read().strip()
