a57afbaf46c305d0a2091873a45024bfdc6e72f5
Andreas Jung <yet@gmx.de>
2008-12-12 07:19:26 +0000

    better support for SQLAlchemy declarative layer

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ CLASSIFIERS = [
     'Topic :: Software Development :: Libraries :: Python Modules',
 ]
 
-version = '1.3.6'
+version = '1.3.7'
 
 desc = open('README.txt').read().strip()
 changes = open('CHANGES.txt').read().strip()
