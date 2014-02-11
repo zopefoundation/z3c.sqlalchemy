018f41e4897b87bb93f1c645d38928d05703bb50
Martin Peeters <martin.peeters@affinitic.be>
2014-02-11 16:19:23 +0100

    Flake8 fix

--- src/z3c/sqlalchemy/tests/testSQLAlchemy.py
+++ src/z3c/sqlalchemy/tests/testSQLAlchemy.py
@@ -21,8 +21,6 @@ import tempfile
 
 from sqlalchemy import MetaData, Integer, String, Column, Table
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relation
-from sqlalchemy.schema import ForeignKey
 try:
     from sqlalchemy import exceptions
 except ImportError:
@@ -49,14 +47,14 @@ class WrapperTests(unittest.TestCase):
         self.db = wrapper = createSAWrapper(self.dsn)
         metadata = MetaData(bind=wrapper.engine)
 
-        users = Table('users', metadata,
-                      Column('id', Integer, primary_key=True),
-                      Column('firstname', String(255)),
-                      Column('lastname', String(255)))
+        Table('users', metadata,
+              Column('id', Integer, primary_key=True),
+              Column('firstname', String(255)),
+              Column('lastname', String(255)))
 
-        skill = Table('skills', metadata,
-                      Column('user_id', Integer, primary_key=True),
-                      Column('name', String(255)))
+        Table('skills', metadata,
+              Column('user_id', Integer, primary_key=True),
+              Column('name', String(255)))
 
         metadata.create_all()
 
@@ -68,11 +66,10 @@ class WrapperTests(unittest.TestCase):
             metadata.drop_all()
 
     def testIFaceZopePostgres(self):
-        verifyClass(ISQLAlchemyWrapper , ZopePostgresWrapper)
+        verifyClass(ISQLAlchemyWrapper, ZopePostgresWrapper)
 
     def testIModel(self):
         verifyClass(IModel, Model)
-
 
     def testSimplePopulation(self):
         db = createSAWrapper(self.dsn)
@@ -92,12 +89,11 @@ class WrapperTests(unittest.TestCase):
         self.assertEqual(len(rows), 2)
         row1 = rows[0]
         d = row1.asDict()
-        self.assertEqual(d, {'firstname' : 'udo', 'lastname' : 'juergens', 'id' : 1})
-
+        self.assertEqual(d, {'firstname': 'udo', 'lastname': 'juergens', 'id': 1})
 
     def testMapperWithCustomModel(self):
 
-        class myUser(MappedClassBase): 
+        class myUser(MappedClassBase):
             pass
 
         M = Model()
@@ -112,28 +108,25 @@ class WrapperTests(unittest.TestCase):
             'mytable', self.db.metadata,
             Column('id', Integer, primary_key=True),
         )
+
         class MyClass(object):
             pass
         mapper = sqlalchemy.orm.mapper(MyClass, mytable)
         self.db.registerMapper(mapper, 'mymapper')
 
-
 #    def testCustomMapperClassWithWrongType(self):
 #
-#        class myUser(object): 
+#        class myUser(object):
 #            pass
 #
 #        M = Model()
 #        self.assertRaises(TypeError, M.add, 'users', mapper_class=myUser)
 
-
     def testGetMappers(self):
-
         db = createSAWrapper(self.dsn)
-        Users = db.getMapper('users')
-        Skills = db.getMapper('skills')
-        User, Skills = db.getMappers('users', 'skills')
-
+        db.getMapper('users')
+        db.getMapper('skills')
+        db.getMappers('users', 'skills')
 
     def testModelWeirdParameters(self):
         M = Model()
@@ -148,10 +141,9 @@ class WrapperTests(unittest.TestCase):
         M.add('non_existing_table')
         db = createSAWrapper(self.dsn, model=M)
         try:
-            foo = db.getMapper('non_existing_table')
+            db.getMapper('non_existing_table')
         except exceptions.NoSuchTableError:
             pass
-
 
     def testWrapperRegistration(self):
         wrapper = createSAWrapper(self.dsn)
@@ -159,28 +151,24 @@ class WrapperTests(unittest.TestCase):
         wrapper2 = getSAWrapper('test.wrapper1')
         self.assertEqual(wrapper, wrapper2)
 
-
     def testWrapperRegistrationFailing(self):
-        wrapper = createSAWrapper(self.dsn)
+        createSAWrapper(self.dsn)
         self.assertRaises(ValueError, getSAWrapper, 'test.wrapperNonExistant')
-
 
     def testWrapperDoubleRegistrationFailing(self):
         wrapper = createSAWrapper(self.dsn)
         registerSAWrapper(wrapper, 'test.wrapper2')
         self.assertRaises(ValueError, registerSAWrapper, wrapper, 'test.wrapper2')
 
-
     def testWrapperDirectRegistration(self):
         wrapper = createSAWrapper(self.dsn, name='test.wrapper3')
         wrapper2 = getSAWrapper('test.wrapper3')
         self.assertEqual(wrapper, wrapper2)
 
-
     def testCheckConnection(self):
         """ Check access to low-level connection """
         db = createSAWrapper(self.dsn)
-        conn = db.connection               
+        conn = db.connection
         cursor = conn.cursor()
         cursor.execute('select * from users')
         rows = cursor.fetchall()
@@ -201,13 +189,12 @@ class WrapperTests(unittest.TestCase):
         rows = cursor.fetchall()
         self.assertEqual(len(rows), 2)
 
-
     def testDeclarative(self):
 
         db = createSAWrapper(self.dsn)
         session = db.session
         metadata = db.metadata
-        Base = declarative_base(metadata = db.metadata)
+        Base = declarative_base(metadata=metadata)
 
         class Foo(Base):
             __tablename__ = 'foo'
@@ -223,7 +210,6 @@ class WrapperTests(unittest.TestCase):
 
         rows = session.query(Foo).all()
         self.assertEqual(len(rows), 2)
-
 
     def testDeclarativeWithModel(self):
         def getModel(metadata):
@@ -252,9 +238,9 @@ class WrapperTests(unittest.TestCase):
         rows = session.query(Foo).all()
         self.assertEqual(len(rows), 2)
 
+
 def test_suite():
     from unittest import TestSuite, makeSuite
     suite = TestSuite()
     suite.addTest(makeSuite(WrapperTests))
     return suite
-
