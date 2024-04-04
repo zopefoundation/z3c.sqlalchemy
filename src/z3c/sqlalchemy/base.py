zopefoundation/z3c.sqlalchemy

commit fb7d533aae819dc7e1aef8322c2f2d60a9a1b5d0

Support SQLAlchemy 2.0 (#16)
Fixes https://github.com/zopefoundation/z3c.sqlalchemy/issues/15

src/z3c/sqlalchemy/base.py
@@ -96,13 +96,13 @@ class ZopeWrapper:
     @property
     def metadata(self):
         if not hasattr(self, '_v_metadata'):
-            self._v_metadata = MetaData(self._engine)
+            self._v_metadata = MetaData()
         return self._v_metadata
 
     @property
     def session(self):
         """ Return thread-local session """
-        return self._sessionmaker()
+        return self._session
 
     @property
     def connection(self):
@@ -140,3 +140,4 @@ class ZopeWrapper:
                                             autoflush=True,
                                             **self.session_options))
         register(self._sessionmaker)
+        self._session = self._sessionmaker()

setup.py
@@ -66,7 +66,7 @@ setup(name='z3c.sqlalchemy',
       python_requires='>=3.7',
       install_requires=[
           'setuptools',
-          'SQLAlchemy>=0.5.5, < 2',
+          'SQLAlchemy>=1.4',
           'zope.sqlalchemy>=1.2.0',
           'zope.component',
           'zope.interface',

src/z3c/sqlalchemy/mapper.py
@@ -14,8 +14,8 @@ import threading
 from sqlalchemy import Table
 from sqlalchemy.ext.declarative import DeclarativeMeta
 from sqlalchemy.orm import class_mapper
-from sqlalchemy.orm import mapper
-from sqlalchemy.orm import relation
+from sqlalchemy.orm import registry
+from sqlalchemy.orm import relationship
 
 
 marker = object
@@ -112,8 +112,7 @@ class MapperFactory:
                           (MappedClassBase,), {})
         else:
             newCls = cls
-
-        mapper(newCls, table, properties=properties)
+        registry().map_imperatively(newCls, table, properties=properties)
         return newCls
 
 
@@ -163,7 +162,7 @@ class LazyMapperCollection(dict):
                 table = Table(tablename,
                               self._metadata,
                               schema=schema,
-                              autoload=True)
+                              autoload_with=self._engine)
 
             # check if the model contains an optional mapper class
             mapper_class = None
@@ -209,8 +208,11 @@ class LazyMapperCollection(dict):
 
                 # add the mapper as relation to the properties dict
                 properties[table_refname] = (
-                    relation(table_ref_mapper,
-                             cascade=self._model.get(name, {}).get('cascade')))
+                    relationship(
+                        table_ref_mapper,
+                        cascade=self._model.get(name, {}).get('cascade'),
+                    )
+                )
 
             # create a mapper and cache it
             if mapper_class and 'c' in mapper_class.__dict__:

src/z3c/sqlalchemy/tests/test_SQLAlchemy.py
@@ -23,7 +23,7 @@ from sqlalchemy import MetaData
 from sqlalchemy import String
 from sqlalchemy import Table
 from sqlalchemy import exc
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from zope.interface.verify import verifyClass
 
 from z3c.sqlalchemy import Model
@@ -46,7 +46,7 @@ class WrapperTests(unittest.TestCase):
             self.tempfile = tempfile.mktemp()
             self.dsn = 'sqlite:///%s' % self.tempfile
         self.db = wrapper = createSAWrapper(self.dsn)
-        metadata = MetaData(bind=wrapper.engine)
+        metadata = MetaData()
 
         Table('users', metadata,
               Column('id', Integer, primary_key=True),
@@ -57,7 +57,7 @@ class WrapperTests(unittest.TestCase):
               Column('user_id', Integer, primary_key=True),
               Column('name', String(255)))
 
-        metadata.create_all()
+        metadata.create_all(bind=wrapper.engine)
 
     def tearDown(self):
         if self.tempfile:
@@ -114,7 +114,8 @@ class WrapperTests(unittest.TestCase):
 
         class MyClass:
             pass
-        mapper = sqlalchemy.orm.mapper(MyClass, mytable)
+        registry = sqlalchemy.orm.registry()
+        mapper = registry.map_imperatively(MyClass, mytable)
         self.db.registerMapper(mapper, 'mymapper')
 
 #    def testCustomMapperClassWithWrongType(self):
@@ -234,7 +235,7 @@ class WrapperTests(unittest.TestCase):
                 name = Column('name', String(50))
 
             model.add('foo', mapper_class=Foo)
-            Base.metadata.create_all()
+            Base.metadata.create_all(self.db._engine)
             return model
 
         db = createSAWrapper(self.dsn, model=getModel)

commit feac53aa181ad83add491fabf99d3d9ba6546dac

The session property returns a fresh session everytime it is invoked
Partially revert commit fb7d533aae819dc7e1aef8322c2f2d60a9a1b5d0
which caused the sqlalchemy session instance to be persisted as an instance attribute.
Fixes #21

src/z3c/sqlalchemy/base.py
@@ -102,7 +102,7 @@ class ZopeWrapper:
     @property
     def session(self):
         """ Return thread-local session """
-        return self._session
+        return self._sessionmaker()
 
     @property
     def connection(self):
@@ -140,4 +140,3 @@ class ZopeWrapper:
                                             autoflush=True,
                                             **self.session_options))
         register(self._sessionmaker, **self.extension_options)
-        self._session = self._sessionmaker()